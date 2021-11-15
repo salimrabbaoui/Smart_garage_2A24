a7c680b7e7c2cfa18e778f353d62389505f8136c
oussamabousbih <oussama.bousbih@esprit.tn>
2021-11-15 09:53:32 +0100

    add

--- smtp.h
+++ smtp.h
@@ -1,14 +1,3 @@
-/*
-Copyright (c) 2013 Raivis Strogonovs
-
-http://morf.lv
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.*/
-
 #ifndef SMTP_H
 #define SMTP_H
 
@@ -20,8 +9,8 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 #include <QDebug>
 #include <QtWidgets/QMessageBox>
 #include <QByteArray>
-#include <QFile>
-#include <QFileInfo>
+#include <QtNetwork>
+#include <QSslSocket>
 
 
 
@@ -32,12 +21,12 @@ class Smtp : public QObject
 
 public:
     Smtp( const QString &user, const QString &pass,
-          const QString &host, int port = 465, int timeout = 30000 );
+          const QString &host, quint16 port = 587
+            , int timeout = 30000 );
     ~Smtp();
 
     void sendMail( const QString &from, const QString &to,
-                   const QString &subject, const QString &body,
-                   QStringList files = QStringList());
+                   const QString &subject, const QString &body );
 
 signals:
     void status( const QString &);
@@ -60,7 +49,7 @@ private:
     QString user;
     QString pass;
     QString host;
-    int port;
+    quint16 port;
     enum states{Tls, HandShake ,Auth,User,Pass,Rcpt,Mail,Data,Init,Body,Quit,Close};
     int state;
 
